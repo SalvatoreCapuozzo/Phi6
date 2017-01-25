665ce73d2cdafd34a18bdff86517021d2d7a57a5
Armando Ariemma <armandoariemma@armandos-macbook-pro.local>
2017-01-25 12:38:21 +0100

    porco dio

--- Phi6/CarusiellViewController.swift
+++ Phi6/CarusiellViewController.swift
@@ -57,12 +57,5 @@ class CarusiellViewController: UIViewController, iCarouselDataSource, iCarouselD
         pageControl.currentPage = carousel.currentItemIndex
     }
     
-<<<<<<< Updated upstream
-=======
-    
-    func carousel(_ carousel: iCarousel, didSelectItemAt index: Int) {
-        self.performSegue(withIdentifier: "categorySelected", sender: carousel)
-    }
-    
->>>>>>> Stashed changes
+
 }
