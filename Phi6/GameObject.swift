558068b5da355df3f3cda6928228c294bac2157a
Darkilon <dark92@hotmail.it>
2017-01-27 12:35:34 +0100

    Iniziati i lavori sulle collisioni

---
+++ Phi6/GameObject.swift
@@ -0,0 +1,15 @@
+//
+//  GameObject.swift
+//  Phi6
+//
+//  Created by Gennaro Nappi on 27/01/17.
+//  Copyright © 2017 ITNH. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameObject : SKSpriteNode
+{
+    
+}
